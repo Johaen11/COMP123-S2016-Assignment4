5e600c88ba7f77394a615c6733b7b8658710e191
johaen11 <johngnanakumar@gmail.com>
2016-07-12 20:56:24 -0400

    added for loop

--- COMP123-S2016-Assignment4/Program.cs
+++ COMP123-S2016-Assignment4/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * Author: Johaen Gnanakumar
+ * Date: July 12, 2016
+ * StudentID: 300880309
+ * Description: Added for loop
+ * Version: 0.01
+ */
 namespace COMP123_S2016_Assignment4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dice();
+            
         }
+            
+
+            public static void Dice() {
+            
+            int[] tally = new int[36000];
+
+            for (int counter = 0; counter <= 36000; counter++)
+            {
+                Random rnd = new Random();
+
+                int DiceOne = rnd.Next(1, 7);
+                int DiceTwo = rnd.Next(1, 7);
+
+      tally[36000] = DiceOne + DiceTwo;
+
+            }
+
+        }
+    
     }
 }
